b962002bd79ace460bf6ef85db44b7adc9831dd5
Srushti Wategaonkar <147075959+srushtiw874@users.noreply.github.com>
2024-03-24 16:40:46 +0530

    Update electricityform_connection.php

--- electricityform_connection.php
+++ electricityform_connection.php
@@ -1,18 +1,34 @@
 <?php
-    $servername = "localhost";
+session_start();
+
+$servername = "localhost";
     $username = "root";
     $password = "";
     $database = "ecotrack";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Sorry!! Failed to connect: " . mysqli_connect_error());
+} else {
+    echo "Connection was successful<br>";
+}
+
+if (isset($_SESSION['uname'])) {
+    $uname = $_SESSION['uname'];
     
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    $fetch_email_sql = "SELECT email FROM newuser_credentials WHERE uname = ?";
+    $stmt = $conn->prepare($fetch_email_sql);
+    $stmt->bind_param("s", $uname);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $email = $row['email'];
 
-    if (!$conn) {
-        die("Sorry!! Failed to connect: " . mysqli_connect_error());
-    } else {
-        echo "Connection was successful<br>";
-    }
+    $_SESSION['email'] = $email;
+}
 
-    if (isset($_POST['Submit'])) {
+if (isset($_POST['Submit'])) {
         $persons = $_POST['persons'];
         $energy_efficient_appliances = $_POST['energy_efficient_appliances'];
         $heating_cooling = $_POST['heating_cooling'];
@@ -32,10 +48,24 @@
         $quantity3 = $_POST['quantity3'];
         $duration3 = $_POST['duration3'];
 
-        $sql = $sql = "INSERT INTO  user_electricityform_credentials (persons, energy_efficient_appliances, heating_cooling, laundry_frequency, turn_off_lights, lighting_type, solar_panels, unplug_electronics, units_consumped, device_name, quantity, duration, device_name2, quantity2, duration2, device_name3, quantity3, duration3) 
-        VALUES ('$persons', '$energy_efficient_appliances', '$heating_cooling', '$laundry_frequency', '$turn_off_lights', '$lighting_type', '$solar_panels', '$unplug_electronics', '$units_consumped', '$device_name', '$quantity', '$duration', '$device_name2', '$quantity2', '$duration2', '$device_name3', '$quantity3', '$duration3')";
-        
-        $result = mysqli_query($conn, $sql);
-        
+    if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
+        echo "Email is missing!";
+        exit;
     }
+    
+    $email = $_SESSION['email'];
+
+    $sql = "INSERT INTO user_electricityform_credentials (email, persons, energy_efficient_appliances, heating_cooling, laundry_frequency, turn_off_lights, lighting_type, solar_panels,        unplug_electronics, units_consumped, device_name, quantity, duration, device_name2, quantity2, duration2, device_name3, quantity3, duration3) 
+            VALUES ('$email', '$persons', '$energy_efficient_appliances', '$heating_cooling', '$laundry_frequency', '$turn_off_lights', '$lighting_type', '$solar_panels',            '$unplug_electronics', '$units_consumped', '$device_name', '$quantity', '$duration', '$device_name2', '$quantity2', '$duration2', '$device_name3', '$quantity3', '$duration3')";
+    
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "<script>
+            alert('Form data submitted successfully')
+            window.location.href='dashboard.php'
+            </script>";
+    } else {
+        echo '<script>alert("Some internal error occurred..Try again")</script>';
+    }
+}
 ?>
